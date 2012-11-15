d7bfa0ae3af512b23a63fa95c5ada9f48e782d90
Dimitri Gnidash <dimitri@bnotions.com>
2012-11-15 04:22:29 -0500

    The code ready for the morning

--- workshop2/scale_image/views.py
+++ workshop2/scale_image/views.py
@@ -1,12 +1,4 @@
 from django.views.generic import CreateView
-from stallion.images import tasks
-from stallion.email import tasks as email_tasks
-
-from celery.task import group, chord
-
-from celery.task.sets import TaskSet
-import json
-
 
 from forms import NotifyScaledImageForm
 
@@ -24,24 +16,5 @@ class ScaleImageView(CreateView):
     def form_valid(self, form):
         super(ScaleImageView, self).form_valid(form)
 
-        subtasks=[tasks.scale_image.si(self.object.image.name, 50),
-                    tasks.scale_image.si(self.object.image.name, 100),
-                    tasks.scale_image.si(self.object.image.name, 150),
-                    tasks.scale_image.si(self.object.image.name, 200),
-                    tasks.scale_image.si(self.object.image.name, 300),
-                    tasks.scale_image.si(self.object.image.name, 450)]
-
-        upon_completion = email_tasks.send_email.si("dimitri@bnotions.com",
-                                                    [self.object.notify],
-                                                    "Yo",
-                                                    "All your images are scaled")
-
-        subtasks_async = group(subtasks).apply_async()  # (upon_completion)
-
-        chord(subtasks)(upon_completion)
-
-        task_ids = [t.task_id for t in subtasks_async.subtasks]
-
         return self.render_to_response(self.get_context_data(form=form,
-                                        task_ids=json.dumps(task_ids),
-                                        success=True))
+                                       success=True))
